1122047cf59481635b41a7581dc6200ea172afe9
kwakuAgyemang <kwaku.agyeman@ashesi.edu.gh>
2022-02-09 15:19:54 +0000

    Authentication

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="jumbotron">
+    <ul>
+        <li>
+            <a href="{{route('register')}}">Register</a>
+        </li>
+        <!--<li>
+            <a href="{{route('login')}}">Login</a>
+        </li>-->
+    </ul>
+
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +18,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('home');
+
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
