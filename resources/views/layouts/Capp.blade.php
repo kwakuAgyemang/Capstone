7d1ec89a8a90eb65d685d9e430bb5c7161331ab8
kwakuAgyemang <59964154+kwakuAgyemang@users.noreply.github.com>
2022-05-08 17:55:39 +0000

    update

--- resources/views/layouts/Capp.blade.php
+++ resources/views/layouts/Capp.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- CSS only -->
 
-    <link rel="shortcut icon" href="./img/AboboyaExpress-logos.jpeg" type="image/x-icon" />
+    <link rel="shortcut icon" href="{{asset('/storage/images/profile/AboboyaExpress-logos.jpeg' )}}" type="image/x-icon" />
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>AboboyaExpress</title>
 </head>
@@ -23,7 +23,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
         <div class="container">
           <a class="navbar-brand" href="{{route('collector.home')}}">
-            <img src="img/AboboyaExpress-logos.jpeg" alt="..." height="36">
+            <img src="{{asset('/storage/images/profile/AboboyaExpress-logos.jpeg' )}}" alt="..." height="36">
           </a>
           @auth
 
