75708fa311fbdcc031c17a5b234fa46544459aaf
kwakuAgyemang <59964154+kwakuAgyemang@users.noreply.github.com>
2022-05-08 19:51:45 +0000

    update

--- resources/views/dashboard/user/auth/register.blade.php
+++ resources/views/dashboard/user/auth/register.blade.php
@@ -206,7 +206,6 @@
         const phoneInput = window.intlTelInput(phoneInputField, {
             preferredCountries: ["gh"],
             initialCountry: "auto",
-            geoIpLookup: getIp,
             utilsScript:
             "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
         });
